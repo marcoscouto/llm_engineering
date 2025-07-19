0da35fe8b5510463766ea7252befc7055707d2f7
marcoscouto <marcosmartinellicouto@gmail.com>
2025-07-19 13:27:04 -0300

    day5: create brochure content and do the week1 exercise

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-source .venv/bin/activate
+uv venv llms
+source llms/bin/activate
 uv add -r requirements.txt
 uv sync
-uv add --dev ipykernel
-uv run python -m ipykernel install --user --name=llm_engineering_env --display-name="LLM Engineering"
-uv run --with jupyter jupyter lab --KernelSpecManager.default_kernel_name=llm_engineering_env
+uv run jupyter lab
