e01757423b8fe27671f1a536fdcd343dcfa3e3dc
Marcos Couto <marcosmartinellicouto@gmail.com>
2025-07-18 21:35:06 -0300

    build: update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+source .venv/bin/activate
+uv add -r requirements.txt
 uv sync
-uv run --with jupyter jupyter lab
+uv add --dev ipykernel
+uv run python -m ipykernel install --user --name=llm_engineering_env --display-name="LLM Engineering"
+uv run --with jupyter jupyter lab --KernelSpecManager.default_kernel_name=llm_engineering_env
